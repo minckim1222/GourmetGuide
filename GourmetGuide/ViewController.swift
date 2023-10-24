93c68b2eedabfdaf023d28638892ccc00e656cc1
minckim1222 <min.c.kim@gmail.com>
2023-10-24 18:21:59 -0500

    Initial Commit

---
+++ GourmetGuide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GourmetGuide
+//
+//  Created by Min Kim on 10/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
